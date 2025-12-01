8c765b49165fc8fa5dc6abe1e66e11cc6c85332a
Isaac Criddle <isaacjamescriddle@gmail.com>
2025-12-01 13:45:36 -0800

    More added to vis3, basic dropdown setup

--- src/data.js
+++ src/data.js
@@ -8,3 +8,9 @@ export const usmap = fetch("./states-albers-10m.json")
 export const flightdata = d3.csv("../data/flights.csv");
 
 export const airportdata = d3.csv("../data/airports.csv");
+
+export const toydata = {
+    ds1: [{x:0,y:12},{x:0,y:45}],
+    ds2: [{x:0,y:72},{x:0,y:28}],
+    ds3: [{x:0,y:82},{x:0,y:18}]
+}
