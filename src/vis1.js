e4b5512c07c38560ea533a1e59a9b4537f60bc06
Scott Milner <scottdmilner@gmail.com>
2025-12-01 17:11:32 -0800

    reorganize vis1

--- src/vis1.js
+++ src/vis1.js
@@ -12,60 +12,85 @@ const v1_flightdata = d3.csv("../data/v1_flights.csv", row => ({
 
 
 async function buildLookup() {
-    const lookup = {};
-    return airportdata.then(data => data.forEach(
-        row => lookup[row.id] = {
+    return airportdata.then(data => data.reduce((acc, row) => {
+        acc[row.id] = ({
             code: row.code,
             latpx: row.latpx, 
             longpx: row.longpx,
-        }
-    )).then(_ => lookup);
+        });
+        return acc;
+    }, {}));
+}
+
+async function buildConnections() {
+    return v1_flightdata.then(data => data.reduce((acc, row) => {
+        if (!(row.originId in acc)) 
+            acc[row.originId] = [];
+        acc[row.originId].push({
+            destId: row.destId, 
+            counts: row.counts, 
+            delaycounts: row.delaycounts
+        });
+        return acc;
+    }, {}));
 }
 
 async function plotAirports() {
     const lookup = await buildLookup();
+    const conn_index = await buildConnections();
+
     map.append("g")
-        .attr("fill", "brown")
-        .attr("fill-opacity", 0.5)
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 0.5)
         .selectAll()
         .data(await airportdata)
-        .join("circle")
-        .attr("transform", d => `translate(${d.longpx},${d.latpx})`)
-        .attr("r", _d => 5)
-        .on("mouseover", async function (event, ad) {
-            const hoverdata = map.append("g")
-                .attr("class", "hoverdata")
-            const active_label = hoverdata.append("g")
-                .attr("transform", `translate(${ad.longpx},${ad.latpx})`);
+        .join("g")
+        .each(async function(ad) {
+            const group = d3.select(this);
+
+            // Airport connections
+            const connections = group.append("g")
+                .attr("class", "connections")
+                .selectAll()
+                .data(conn_index[ad.id])
+                .join("path")
+                .attr("d", fd => {
+                    const oport = lookup[ad.id];
+                    const dport = lookup[fd.destId];
+                    return `M ${oport.longpx} ${oport.latpx} Q ${(oport.longpx + dport.longpx) / 2} ${(oport.latpx + dport.latpx) / 2 - 30}, ${dport.longpx} ${dport.latpx}`;
+                })
+                .attr("visibility", "hidden");
             
-            // Label
-            const text = active_label.append("text").attr("x", 0).attr("y", 0);
-            text.append("tspan").attr("x", 0).attr("dy", "1.2em")
+            // Airport Label
+            const label = group.append("g")
+                .attr("class", "hoverdata")
+                .attr("transform", `translate(${ad.longpx},${ad.latpx})`)
+                .attr("visibility", "hidden");
+            const text = label.append("text")
+                .attr("x", 0).attr("y", 0)
+            text.append("tspan")
+                .attr("x", 0).attr("dy", "1.2em")
                 .text(ad.code);
-            const {width: bbwidth, height: bbheight} = active_label.node().getBBox();
+            const {width: bbwidth, height: bbheight} = label.node().getBBox();
             const margin = 8;
-            active_label.insert('rect', ':first-child') // create background rect
+            label.insert('rect', ':first-child') // create background rect
                 .attr("x", -margin)
                 .attr("y", 0)
                 .attr("width", bbwidth + 2 * margin)
                 .attr("height", bbheight + 2 * margin);
-            
-            hoverdata.append("g")
-                .attr("stroke", "#444")
-                .attr("stroke-width", 1.5)
-                .attr("fill", "none")
-                .selectAll()
-                .data((await v1_flightdata).filter(fd => fd.originId === ad.id))
-                .join("path")
-                .attr("d", fd => {
-                    const oport = lookup[fd.originId];
-                    const dport = lookup[fd.destId];
-                    return `M ${oport.longpx} ${oport.latpx} Q ${(oport.longpx + dport.longpx) / 2} ${(oport.latpx + dport.latpx) / 2 - 30}, ${dport.longpx} ${dport.latpx}`;
+
+            // Airport circle
+            group.append("circle")
+                .attr("class", "airport")
+                .attr("transform", d => `translate(${d.longpx},${d.latpx})`)
+                .attr("r", _d => 5)
+                .on("mouseover", async function (_event, ad) {
+                    connections.attr("visibility", "visible");
+                    label.attr("visibility", "visible");
+                })
+                .on("mouseout", () => {
+                    connections.attr("visibility", "hidden");
+                    label.attr("visibility", "hidden");
                 });
-        })
-        .on("mouseout", () => map.selectAll(".hoverdata").remove())
+        });
 }
 
 
