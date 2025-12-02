28ab5f2264a222c10e1dcef4e953e3b48500789d
Isaac Criddle <isaacjamescriddle@gmail.com>
2025-12-01 18:10:08 -0800

    Small fixes to vis3, took some notes in README

--- src/vis3.js
+++ src/vis3.js
@@ -1,13 +1,11 @@
-import { airportdata, toydata } from "./data.js";
-import { projection } from "./util.js";
-
+import { airportdata, toydata, airlinedata } from "./data.js";
 
 const container = d3.select('#container3')
 const leftCanvas = d3.select("#leftCanvas");
 
 const color = ['#1459D9', '#daa520'];
-const width = 928;
-const height = 500;
+const leftWidth = +leftCanvas.attr("width");
+const leftHeight = +leftCanvas.attr("height");
 const marginTop = 30;
 const marginRight = 0;
 const marginBottom = 30;
@@ -15,14 +13,14 @@ const marginLeft = 40;
 
 // Declare the x (horizontal position) scale.
 const x = d3.scaleBand()
-    .domain(d3.groupSort(, ([d]) => -d.frequency, (d) => d.letter)) // descending frequency
-    .range([marginLeft, width - marginRight])
+    .domain([0, d3.max(airlinedata, (d) => airlinedata.total)])
+    .range([marginLeft, leftWidth - marginRight])
     .padding(0.1);
 
 // Declare the y (vertical position) scale.
 const y = d3.scaleLinear()
     .domain([0, d3.max(data, (d) => d.frequency)])
-    .range([height - marginBottom, marginTop]);
+    .range([leftHeight - marginBottom, marginTop]);
 
 function updateData(newData) {
     console.log(newData);
