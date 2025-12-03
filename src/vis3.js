e778692e893858bc324cc16c67157273686bb679
Isaac Criddle <isaacjamescriddle@gmail.com>
2025-12-02 22:34:06 -0800

    Added multiple functional bars

--- src/vis3.js
+++ src/vis3.js
@@ -10,16 +10,18 @@ import {
     overalldata
 } from "./data.js";
 
+import { redBlue } from "./util.js";
+
 const container = d3.select('#container3')
 const canvas = d3.select("#canvas");
 
 const color = ['#1459D9', '#daa520'];
 const width = +canvas.attr("width");
 const height = +canvas.attr("height");
-const marginTop = 50;
-const marginRight = 50;
+const marginTop = 100;
+const marginRight = 100;
 const marginBottom = 50;
-const marginLeft = 50;
+const marginLeft = 20;
 const margin = {marginTop, marginRight, marginBottom, marginLeft};
 const barSize = 10;
 
@@ -31,66 +33,108 @@ let overall_dict;
 
 let data_key;
 
-async function drawGraph(newData) {
-    const xscale = d3.scaleLinear()
-        .domain([0, 1])
-        .range([marginLeft, width - marginRight])
+// Declare the y (vertical position) scale.
+const yscale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([height - marginBottom, marginTop]);
 
     // Declare the y (vertical position) scale.
-    const yscale = d3.scaleLinear()
-        .domain([0, 1])
-        .range([height - marginBottom, marginTop]);
+const xscale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([width - marginRight, marginLeft]);
 
+async function drawGraph(newData) {
+    let table = await newData;
     canvas.selectAll("#leftbar").remove();
-    console.log(overall_dict);
-
-    // populate information about the stacked bars
-    let bars_y = [0, overall_dict['cancelled'], overall_dict['delayed']];
-    let c_sum = 0;
-    console.log(bars_y);
-    for(let i = 0; i < bars_y.length; i++) {
-        c_sum += bars_y[i];
-        bars_y[i] = c_sum / overall_dict['total'];
-    }
-    bars_y.push(1);
+    let rows_to_graph = [table[0], table[1]];
+    rows_to_graph.push(overall_dict)
     const labels = ['cancelled', 'delayed', 'on time'];
-    const colors = ['black', '#daa520', '#1459D9'];
+    const colors = [redBlue(1.0), redBlue(0.6), redBlue(0.1)];
 
-    for(let i = 0; i < bars_y.length - 1; i++) {
-        const rInt = Math.floor(i / 3 * 255);
-        const rgbString = colors[i];
-        const y = yscale(bars_y[i + 1]);
-        const height = yscale(bars_y[i]) - yscale(bars_y[i + 1]);
+    
 
-        const barWidth = 50;
+    canvas.append('g')
+        .attr('transform', `translate(10, ${height - margin.bottom})`)
+        .call(d3.axisBottom(xscale.copy().range([marginLeft, width - marginRight])));
 
-        canvas.append("rect")
-            .attr("class", "leftbar")
-            .attr("x", marginLeft)
-            .attr("y", y)
-            .attr("width", barWidth)
-            .attr("height", height)
-            .attr("fill", rgbString)
-            .attr("rx", 2)
-            .attr("ry", 2)
-            .on('mouseover', function (event, d) { // Handle 'mouseover' events
-                const group = canvas.append('g')
-                .attr('class', 'ptLabel3')
-                .attr('transform',
-                    `translate(${marginLeft + barWidth} ${y + height / 2})`
-                );
-                const text = group.append("text").attr("x", 0).attr("y", 0);         // add text element into group
-                text.append("tspan").attr("x", 0).attr("dy", "1.2em").text(labels[i]); // add text spans into text, with line spacing `dy`
-                const bbox = group.node().getBBox();
-                group.insert('rect', ':first-child') // create background rectangle
-                .attr("x", -2)
-                .attr("y",  2)
-                .attr("width", bbox.width + 4)
-                .attr("height", bbox.height + 4);
-            })
-            .on('mouseout', function(event, d) {
-                canvas.selectAll('.ptLabel3').remove()    // Remove all ptLabels
-            });;
+    const barHeight = 30;
+    let key_top = marginTop + rows_to_graph.length * barHeight * 1.5 + 10;
+    canvas.append('text')
+        .text(labels[0])
+        .attr('x', marginLeft)
+        .attr('y', key_top)
+        .attr('fill', colors[0]);
+
+    canvas.append('text')
+        .text(labels[1])
+        .attr('x', marginLeft + 30)
+        .attr('y', key_top + barHeight * 0.5)
+        .attr('fill', colors[1]);
+
+    canvas.append('text')
+        .text(labels[2])
+        .attr('x', marginLeft + 60)
+        .attr('y', key_top + barHeight)
+        .attr('fill', colors[2]);
+    
+
+    for(let row_idx = 0; row_idx < rows_to_graph.length; row_idx++) {
+        let row = rows_to_graph[row_idx];
+        const amts = [
+            row['cancelled'], 
+            row['delayed'], 
+            row['total'] - (row['cancelled'] + row['delayed'])
+        ];
+        console.log(row);
+
+        // populate information about the stacked bars
+        let bars_x = [0, row['cancelled'], row['delayed']];
+        let c_sum = 0;
+        for(let i = 0; i < bars_x.length; i++) {
+            c_sum += bars_x[i];
+            bars_x[i] = c_sum / row['total'];
+        }
+        bars_x.push(1);
+        
+
+        for(let i = 0; i < bars_x.length - 1; i++) {
+            const rInt = Math.floor(i / 3 * 255);
+            const rgbString = colors[i];
+            
+            const x = xscale(1 - bars_x[i]);
+            const y =  marginTop + row_idx * barHeight * 1.5;
+            const width = xscale(1 - bars_x[i + 1]) - xscale(1 - bars_x[i]);
+
+            const rect = canvas.append("rect")
+                .attr("class", "leftbar")
+                .attr("x", x)
+                .attr("y", y)
+                .attr("width", width)
+                .attr("height", barHeight)
+                .attr("fill", rgbString)
+                .attr("rx", 2)
+                .attr("ry", 2)
+                .on('mouseover', function (event, d) { // Handle 'mouseover' events
+                    const group = canvas.append('g')
+                        .attr('class', 'hoverdata');
+                    const text = group.append("text").attr("x", 0).attr("y", 0);         // add text element into group
+                    text.append("tspan").attr("x", 0).attr("dy", "1.2em").text(amts[i] + " flights " + labels[i]); // add text spans into text, with line spacing `dy`
+                    const bbox = group.node().getBBox();
+                    let tooltip_x = x + width * 0.5 - bbox.width * 0.5;
+                    if(tooltip_x < 0) tooltip_x = marginLeft;
+                    group.attr('transform',
+                        `translate(${tooltip_x} ${marginTop + row_idx * barHeight * 1.5})`
+                    );
+                    group.insert('rect', ':first-child') // create background rectangle
+                    .attr("x", -2)
+                    .attr("y",  2)
+                    .attr("width", bbox.width + 4)
+                    .attr("height", bbox.height + 4);
+                })
+                .on('mouseout', function(event, d) {
+                    canvas.selectAll('.hoverdata').remove()    // Remove all ptLabels
+                });
+        }
     }
 }
 
