7db056c61f5db52da0bbaf8efc1b7f0638e6f6de
Scott Milner <scottdmilner@gmail.com>
2025-12-05 23:14:11 -0800

    selection

--- src/vis1.js
+++ src/vis1.js
@@ -1,7 +1,7 @@
 import { airportdata } from "./data.js";
 import { redBlue } from "./util.js";
 
-const getColor = (val) => redBlue(val * 2.0);
+const getColor = val => redBlue(val * 2.0);
 
 
 const map = d3.select("#container1 #map");
@@ -52,11 +52,63 @@ let airportDOMIndex = {};
 function setVisible(id, value) {
     airportDOMIndex[id].connections
         .attr("opacity", value ? 1.0 : 0.03)
-        .attr("stroke-width", value ? 3.0 : 1.5)
+        .attr("stroke-width", value ? 4.0 : 1.5)
         .raise();
     airportDOMIndex[id].label
         .attr("visibility", value ? "visible" : "hidden");
 }
+
+function updateSidebar(airportDatum, lookup, conn_index) {
+    // Update sidebar
+    sidebar.select("#airportname")
+        .text(airportDatum.dispName);
+
+    let conn_data = conn_index[airportDatum.id];
+    const total_flights = conn_data.reduce((acc, path) => acc + path.count, 0);
+    const total_delays = conn_data.reduce((acc, path) => acc + path.delaycount, 0);
+    sidebar.select("#summary")
+        .text(`${total_delays} flights delayed of ${total_flights} total flights (${(100 * total_delays / total_flights).toFixed(1)})%`);
+        
+    const barWidth = 100;
+    sidebar.select("#routetable")
+        .selectAll("tr")
+        .data(conn_data.sort((a, b) => b.delayfrac - a.delayfrac), d => d.destId)
+        .join(
+            enter => {
+                let tr = enter.append("tr");
+                tr.attr("title", d => `${lookup[d.destId].dispName} (${d.delaycount}/${d.count})`);
+                tr.append("td")
+                    .text(d => lookup[d.destId].code)
+                let val = tr.append("td")
+                    .attr("width", barWidth);
+                val.append("div")
+                    .attr("class", "inlinebar")
+                    .style("width", d => barWidth * d.delayfrac)
+                    .style("background-color", d => getColor(d.delayfrac))
+                    .style("margin-left", "4px");
+                tr.append("td")
+                    .text(d => `${(100 * d.delayfrac).toFixed(1)}%`);
+                return tr;
+            }, 
+            update => {
+                update.attr("title", d => `${lookup[d.destId].dispName} (${d.delaycount}/${d.count})`);
+                update.select(":nth-child(2)").select("div")
+                    .style("width", d => barWidth * d.delayfrac)
+                    .style("background-color", d => getColor(d.delayfrac));
+                update.select(":nth-child(3)")
+                    .text(d => `${(100 * d.delayfrac).toFixed(1)}%`);
+                return update;
+            }
+        );
+    //@ts-ignore
+    const headerHeight = sidebar.select("#sidebarheader").node().offsetHeight;
+    //@ts-ignore
+    const mapHeight = sidebar.node().offsetHeight;
+    sidebar.select("#routes")
+        .style("height", `calc(${mapHeight}px - ${headerHeight}px - 36px)`);
+}
+
+
 
 async function plotAirports() {
     // await our lookup table builds simultaneously
@@ -91,17 +143,16 @@ async function plotAirports() {
                     d3.select(this)
                         .attr("d",  // set route path
                             `M ${oport.longpx} ${oport.latpx} `
-                            + `Q ${(oport.longpx + dport.longpx) / 2} ${(oport.latpx + dport.latpx) / 2 - 30}, `
+                            + `Q ${(oport.longpx + dport.longpx) / 2} ${(oport.latpx + dport.latpx) / 2 - 50}, `
                             + `${dport.longpx} ${dport.latpx}`
                         )
                         .attr("stroke", getColor(flightDatum.delayfrac))
-                        .on("mouseover", function() {
+                        .on("mouseover", function(event) {
                             if (selectedAirport !== airportDatum.id) return;
-                            
                             // Draw label
                             label = map.select(".labels").append("g")
                                 .attr("class", "hoverdata")
-                                .attr("transform", `translate(${(oport.longpx + dport.longpx) / 2},${(oport.latpx + dport.latpx) / 2})`)
+                                .attr("transform", `translate(${event.offsetX},${event.offsetY})`);
                             const text = label.append("text")
                                 .attr("x", 0).attr("y", 0)
                             text.append("tspan")
@@ -115,11 +166,8 @@ async function plotAirports() {
                                 .attr("y", -margin)
                                 .attr("width", width + 2 * margin)
                                 .attr("height", height + 2 * margin);
-                            console.log(label);
                         })
-                        .on("mouseout", function() {
-                            label?.remove();
-                        });
+                        .on("mouseout", () => label?.remove());
                 });
 
             // Airport labels
@@ -147,13 +195,16 @@ async function plotAirports() {
                 .attr("r", _d => 5)
                 .on("mouseover", function(_event, _data) {
                     setVisible(airportDatum.id, true);
+                    if (selectedAirport === null) {
+                        updateSidebar(airportDatum, lookup, conn_index);
+                    }
                 })
                 .on("mouseout", function(_event, _data) {
                     if (selectedAirport !== airportDatum.id) {
                         setVisible(airportDatum.id, false);
                     }
                 })
-                .on("click", function(_event, _data) {
+                .on("click", function() {
                     if (selectedAirport === airportDatum.id) {
                         selectedAirport = null;
                         return;
@@ -163,46 +214,7 @@ async function plotAirports() {
                         setVisible(selectedAirport, false);
                     }
                     selectedAirport = airportDatum.id;
-
-                    // Update sidebar
-                    sidebar.select("#airportname")
-                        .text(airportDatum.dispName);
-                    
-                    let conn_data = conn_index[airportDatum.id];
-                    const total_flights = conn_data.reduce((acc, path) => acc + path.count, 0);
-                    const total_delays = conn_data.reduce((acc, path) => acc + path.delaycount, 0);
-                    sidebar.select("#summary")
-                        .text(`${total_delays} flights delayed of ${total_flights} total flights (${(100 * total_delays / total_flights).toFixed(1)})%`);
-                    
-                    sidebar.select("#routes")
-                        .selectAll("tr")
-                        .data(conn_data.sort((a, b) => b.delayfrac - a.delayfrac), d => d.destId)
-                        .join(
-                            enter => {
-                                let tr = enter.append("tr");
-                                tr.attr("title", d => lookup[d.destId].dispName);
-                                tr.append("td")
-                                    .text(d => lookup[d.destId].code)
-                                let val = tr.append("td")
-                                    .attr("width", 110);
-                                val.append("div")
-                                    .attr("class", "inlinebar")
-                                    .style("width", d => 110 * d.delayfrac)
-                                    .style("background-color", d => getColor(d.delayfrac))
-                                    .style("margin-left", "4px");
-                                tr.append("td")
-                                    .text(d => `${(100 * d.delayfrac).toFixed(1)}% (${d.delaycount}/${d.count})`);
-                                return tr;
-                            }, 
-                            update => {
-                                update.select(":nth-child(2)").select("div")
-                                    .style("width", d => 110 * d.delayfrac)
-                                    .style("background-color", d => getColor(d.delayfrac));
-                                update.select(":nth-child(3)")
-                                    .text(d => `${(100 * d.delayfrac).toFixed(1)}% (${d.delaycount}/${d.count})`);
-                                return update;
-                            }
-                        );
+                    updateSidebar(airportDatum, lookup, conn_index);
                 });
         });
     
